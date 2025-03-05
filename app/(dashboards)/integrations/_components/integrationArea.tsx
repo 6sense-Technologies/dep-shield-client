400d314bda04d4a16c3f0f9348dc65cc6f02bbc1
Khan Atik Faisal <khanatik1176@gmail.com>
2025-03-05 20:38:58 +0600

    Fixed-Client-Reported-Bugs

--- app/(dashboards)/integrations/_components/integrationArea.tsx
+++ app/(dashboards)/integrations/_components/integrationArea.tsx
@@ -31,7 +31,6 @@ const IntegrationArea: FC<IntegrationAreaProps> = ({ connections, handleConnect,
     }
 
     const handleDisconnectRoute = async () => {
-
         const res = await axios.get(`${TEMP_BACKEND_URL}/github-app/disconnect`,
             {
                 headers: {
@@ -49,11 +48,11 @@ const IntegrationArea: FC<IntegrationAreaProps> = ({ connections, handleConnect,
 
     return (
         <div className="flex-grow flex flex-col items-center md:justify-center pt-20 md:pt-0 px-3 lg:px-6">
-            <div className="flex flex-col items-center justify-center h-96 w-full max-w-[512px] p-12 border rounded-lg gap-y-6">
+            <div className="flex flex-col items-center justify-center h-96 w-full max-w-[512px] p-6 md:p-12 border rounded-lg gap-y-6">
                 <p className="text-sm font-normal text-inputFooterColor">
                     Connect an integration to effortlessly scan your repositories for vulnerabilities, license compliance, and community health.
                 </p>
-                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14">
+                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14 cursor-pointer" onClick={() => gitStatus ? handleDisconnectRoute() : handleConnect("github")}>
                     <Image src={Github} alt="githubLogo" width={100} height={100} />
                     <div className="w-full text-right">
                         {gitStatus ? (
@@ -61,55 +60,35 @@ const IntegrationArea: FC<IntegrationAreaProps> = ({ connections, handleConnect,
                                 <div className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md bg-lightAquaBg p-2 cursor-pointer">
                                     <span onClick={(e) => handleConnect("github", e)}><RefreshCw size={16} /></span>
                                 </div>
-                                <Button variant="none" size="minixs" onClick={() => handleDisconnectRoute()}>
+                                <Button variant="none" size="minixs">
                                     <span className="hidden md:inline">Disconnect</span>
                                     <Link2Off size={16} />
                                 </Button>
                             </div>
                         ) : (
-                            <Button variant="none" size="minixs" onClick={() => handleConnect("github")}>
-                                <span className="hidden md:inline">Connect</span>
+                            <Button variant="none" size="minixs">
+                                <span className="inline">Connect</span>
                                 <Link2 size={16} />
                             </Button>
                         )}
                     </div>
                 </div>
-                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14">
+                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14 cursor-not-allowed opacity-50">
                     <Image src={GitLab} alt="gitLabLogo" width={100} height={100} />
                     <div className="w-full text-right">
-                        {connections.gitlab ? (
-                            <Button variant="none" size="minixs" onClick={() => handleDisconnect("gitlab")}>
-                                <div className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md bg-lightAquaBg p-2">
-                                    <RefreshCw size={16} />
-                                </div>
-                                <span className="hidden md:inline">Disconnect</span>
-                                <Link2Off size={16} className="ml-2" />
-                            </Button>
-                        ) : (
-                            <Button variant="none" size="minixs" disabled>
-                                <span className="hidden md:inline">Connect</span>
-                                <Link2 size={16} />
-                            </Button>
-                        )}
+                        <Button variant="none" size="minixs" disabled>
+                            <span className="inline">Connect</span>
+                            <Link2 size={16} />
+                        </Button>
                     </div>
                 </div>
-                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14">
+                <div className="border p-4 flex justify-between items-center w-full rounded-md h-14 cursor-not-allowed opacity-50">
                     <Image src={Bitbucket} alt="bitBucketLogo" width={100} height={100} />
                     <div className="w-full text-right">
-                        {connections.bitbucket ? (
-                            <Button variant="none" size="minixs">
-                                <div className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md bg-lightAquaBg p-2">
-                                    <RefreshCw size={16} />
-                                </div>
-                                <span className="hidden md:inline">Disconnect</span>
-                                <Link2Off size={16} className="ml-2" />
-                            </Button>
-                        ) : (
-                            <Button variant="none" size="minixs" disabled>
-                                <span className="hidden md:inline">Connect</span>
-                                <Link2 size={16} />
-                            </Button>
-                        )}
+                        <Button variant="none" size="minixs" disabled>
+                            <span className="inline">Connect</span>
+                            <Link2 size={16} />
+                        </Button>
                     </div>
                 </div>
             </div>
