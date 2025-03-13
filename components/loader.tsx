bb0c6a534b86edb3ba673a4ed93c8b28f59cd267
Khan Atik Faisal <khanatik1176@gmail.com>
2025-03-13 12:59:11 +0600

    Loader-with-comment-added

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,10 +1,18 @@
 import { CircleNotch } from '@phosphor-icons/react';
 import React from 'react';
 
-const Loader = () => {
+interface LoaderProps {
+  text?: string;
+  variant?: 'big';
+}
+
+const Loader: React.FC<LoaderProps> = ({ text, variant }) => {
+  const size = variant === 'big' ? 60 : 40;
+
   return (
-    <div className='flex h-screen items-center justify-center w-full'>
-      <CircleNotch size={40} className='animate-spin text-primary' />
+    <div className='flex h-screen w-full flex-col items-center justify-center'>
+      <CircleNotch size={size} className='animate-spin text-primary' />
+      {text ? <p className='pt-4 text-inputFooterColor text-sm font-normal'>{text}</p> : null}
     </div>
   );
 };
