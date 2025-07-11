40b217af82edae20e7947d5a110079bbf56fd63f
6senseHQ <hello@6sensehq.com>
2025-07-11 16:19:52 +0600

    tabs fixed

--- app/(dashboards)/vulnerabilities/[id]/_components/SeverityTabs.tsx
+++ app/(dashboards)/vulnerabilities/[id]/_components/SeverityTabs.tsx
@@ -7,14 +7,13 @@ import { Tabs } from '@mantine/core';
 
 type SeverityKey = 'cvssMetricV2' | 'cvssMetricV30' | 'cvssMetricV31' | 'cvssMetricV40';
 
-
 type Props = {
   severity: VulnerabilityDetailsType['severity'] | undefined;
   activeTab: string | null;
-  setActiveTab: (value: string  | null) => void;
+  setActiveTab: (value: string | null) => void;
 };
 
-const severityLabelMap: Record<string, string> = {
+const severityLabelMap: Record<SeverityKey, string> = {
   cvssMetricV2: 'CVSS2',
   cvssMetricV30: 'CVSS3',
   cvssMetricV31: 'CVSS3.1',
@@ -24,19 +23,17 @@ const severityLabelMap: Record<string, string> = {
 export default function SeverityTabs({ severity, activeTab, setActiveTab }: Props) {
   if (!severity) return null;
 
+  const keys = Object.keys(severity).filter(Boolean).reverse() as SeverityKey[];
+  if (!keys.length) return null;
 
-
-  const keys = Object.keys(severity).toReversed() as SeverityKey[];
-  console.log(' SeverityTabs - keys:', keys)
   const defaultTab = keys[0];
 
-
-    function formatKey(key: string) {
-        return key
-            .replace(/([A-Z])/g, ' $1')
-            .replace(/^./, str => str.toUpperCase())
-            .replace(/Impact$/, ' Impact');
-    }
+  function formatKey(key: string) {
+    return key
+      .replace(/([A-Z])/g, ' $1')
+      .replace(/^./, str => str.toUpperCase())
+      .replace(/Impact$/, ' Impact');
+  }
 
   return (
     <section>
@@ -47,42 +44,39 @@ export default function SeverityTabs({ severity, activeTab, setActiveTab }: Prop
         // onChange={setActiveTab}
         defaultValue={defaultTab}
       >
-        <Tabs.List >
-          {keys.map((key, idx) => {
-            if (!severity[key]) return null;
-            console.log('aaaaaaaaaa',(keys.length - 1 - idx).toString());
-            
-            return (
-              <Tabs.Tab
-                key={(keys?.length - 1 - idx)}
-                value={(keys?.length - 1 - idx).toString()}
-                className="data-[active=true]:text-black"
-              >
-                {severityLabelMap[key] || ''}
-              </Tabs.Tab>
-            );
-          })}
+        <Tabs.List>
+          {keys.map(key => (
+            <Tabs.Tab
+              key={key}
+              value={key}
+              className="data-[active=true]:text-black"
+            >
+              {severityLabelMap[key] || key}
+            </Tabs.Tab>
+          ))}
         </Tabs.List>
 
-        {keys.map((key, idx) => {
+        {keys.map(key => {
           const data = severity[key]?.cvssData;
-          if (!data) return null;
-
           return (
-            <Tabs.Panel key={idx} value={(keys?.length - 1 - idx).toString()} className="flex flex-col gap-3 mt-4 pl-4">
-              {Object.entries(data).map(([k, v]) => (
-                <div key={k} className="grid grid-cols-3">
-                  <span className="font-normal text-sm text-black">{formatKey(k)}</span>
-                  <span
-                    className={cn("text-sm", {
-                      'text-red-500': ['NETWORK', 'NONE', 'LOW', 'HIGH'].includes(String(v)),
-                      'text-green-500': v === 'UNCHANGED',
-                    })}
-                  >
-                    {capitalizeOnlyFirstLetter(String(v ?? ''))}
-                  </span>
-                </div>
-              ))}
+            <Tabs.Panel key={key} value={key} className="flex flex-col gap-3 mt-4 pl-4">
+              {data && Object.entries(data).length ? (
+                Object.entries(data).map(([k, v]) => (
+                  <div key={k} className="grid grid-cols-3">
+                    <span className="font-normal text-sm text-black">{formatKey(k)}</span>
+                    <span
+                      className={cn("text-sm", {
+                        'text-red-500': ['NETWORK', 'NONE', 'LOW', 'HIGH'].includes(String(v)),
+                        'text-green-500': v === 'UNCHANGED',
+                      })}
+                    >
+                      {capitalizeOnlyFirstLetter(String(v ?? ''))}
+                    </span>
+                  </div>
+                ))
+              ) : (
+                <span>No Results</span>
+              )}
             </Tabs.Panel>
           );
         })}
